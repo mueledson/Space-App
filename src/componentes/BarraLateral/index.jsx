d5c2dd9dd28996f19656394e6d443d36be6e2173
edson@app100.com.br <mueledson>
2023-08-30 20:40:45 -0300

    mudamos as fontes

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,44 +1,54 @@
-// import { styled } from "styled-components"
-
-// const HeaderEstilizado = styled.header`
-// `;
+import { styled } from "styled-components"
+import ItemNavegacao from "./itemNavegacao";
+const ListaEstilizado = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    max-width: 212px;
+    
+    li a{
+        text-decoration: none;
+    }
+`;
 
 const BarraLateral = () => {
 
   return (
         <aside>
             <nav>
-                <ul>
-                    <li>
-                        <a href="">
-                            Início
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="">
-                            Mais vistas
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="">
-                            Mais curtidas
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="">
-                            Novas
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="">
-                            Surpreenda-me
-                        </a>
-                    </li>
-                </ul>
+                <ListaEstilizado>
+                    <ItemNavegacao
+                        iconeAtivo="/icones/home-ativo.png"
+                        iconeInativo="/icones/home-inativo.png"
+                        ativo={true}
+                    >
+                        Início
+                    </ItemNavegacao>                        
+                    <ItemNavegacao
+                        iconeAtivo="/icones/mais-vistas-ativo.png"
+                        iconeInativo="/icones/mais-vistas-inativo.png"
+                    >
+                        Mais Vistas
+                    </ItemNavegacao>                        
+                    <ItemNavegacao
+                        iconeAtivo="/icones/mais-curtidas-ativo.png"
+                        iconeInativo="/icones/mais-curtidas-inativo.png"
+                    >
+                        Mais curtidas
+                    </ItemNavegacao>                        
+                    <ItemNavegacao
+                        iconeAtivo="/icones/novas-ativo.png"
+                        iconeInativo="/icones/novas-inativo.png"
+                    >
+                        Novas
+                    </ItemNavegacao>                        
+                    <ItemNavegacao
+                        iconeAtivo="/icones/surpreenda-me-ativo.png"
+                        iconeInativo="/icones/surpreenda-me-inativo.png"
+                    >
+                        Surpreenda-me
+                    </ItemNavegacao>                        
+                </ListaEstilizado>
             </nav>
         </aside>
   )
